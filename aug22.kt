ffcdbfce2a8a747a1a14332d23995f91ace1dfd7
uma324 <117091333+uma324@users.noreply.github.com>
2024-08-26 13:55:29 +0530

    Add files via upload

---
+++ aug22.kt
@@ -0,0 +1,61 @@
+import java.security.KeyStore.TrustedCertificateEntry
+
+
+/*fun main() {
+    var a = 10
+    var b = 20
+    // arithmetic operators
+    val sum = a + b
+    println("sum of $a + $b = $sum")
+    val sub = a - b
+    println("sub of $a - $b = $sub")
+    val mul = a * b
+    println("mul of $a * $b = $mul")
+    val div = a / b
+    println("div of $a / $b = $div")
+    val mod = a % b
+    println("mod of $a % $b = $mod")
+}*/
+//relational operators
+/*fun main() {
+    var a = 20
+    var b = 40
+    println("$a is greater than $b : ${a>b}")
+    println("$a is less than $b : ${a<b}")
+    println("$a is greater than or equal to $b : ${a>=b}")
+    println("$a is less  than or equal to  $b : ${a<=b}")
+    println("$a is equal to $b : ${a==b}")
+    println("$a is not equal to $b : ${a!=b}")
+
+}*/
+
+//Assignment operators
+/*fun main() {
+    var a = 20
+    var b = 3
+    a+=b
+    println(a)
+    a-=b
+    println(a)
+    a*=b
+    println(a)
+    a/=b
+    println(a)
+    a%=b
+    println(a)
+
+}*/
+//unary operators
+/*fun main(){
+    var a = 10
+    val b = false
+    println("unary plus :  ${+a}")
+    println("unary minus :  ${-a}")
+    println("increment by 1 :  ${++a}")
+    println("decrement by 1 :  ${--a}")
+    println("not :  ${!b}")
+}*/
+
+//Bitwise operators
+
+
